1af69906b255a6b00e237e1f790b6425d0882dda
Andrew Kelley <andrew@ziglang.org>
2025-08-11 13:57:04 -0700

    update build script

--- build.zig
+++ build.zig
@@ -3,15 +3,14 @@ const std = @import("std");
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
-    const use_llvm = b.option(bool, "use-llvm", "use the LLVM backend");
 
     const exe = b.addExecutable(.{
         .name = "tetris",
-        .root_source_file = b.path("src/main.zig"),
-        .optimize = optimize,
-        .target = target,
-        .use_llvm = use_llvm,
-        .use_lld = use_llvm,
+        .root_module = b.createModule(.{
+            .root_source_file = b.path("src/main.zig"),
+            .optimize = optimize,
+            .target = target,
+        }),
     });
 
     exe.linkLibC();
